7dea11b20b32a84056ca9be0fa2e68b1ec31ffd5
Andre Lademann <andre.lademann@netresearch.de>
2020-06-08 12:28:44 +0200

    Use jpeg for pwa app icon

--- gridsome.config.js
+++ gridsome.config.js
@@ -39,7 +39,7 @@ module.exports = {
 			use: 'gridsome-plugin-manifest',
 			options: {
 				background_color: '#006eb8',
-				icon_path: './src/assets/img/touchicon.png',
+				icon_path: './src/assets/img/touchicon.jpg',
 				name: 'blugento leaderboard',
 				short_name: 'leaderboard',
 				display: 'standalone',
