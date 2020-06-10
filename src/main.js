54bc2c98387cad18f1240be488e896fbdeb84fcc
Andre Lademann <andre.lademann@netresearch.de>
2020-06-11 00:12:04 +0200

    Fix path to style and layout files

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '/layouts/Default.vue'
-import '/assets/scss/styles.scss'
+import DefaultLayout from './layouts/Default.vue'
+import './assets/scss/styles.scss'
 const appConfig = require('../app.config')
 
 export default function(Vue, { router, head, isClient }) {
