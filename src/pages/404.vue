195d0d1bb19f35c767bb48fd03e3e3310c2f2aae
Andre Lademann <andre.lademann@netresearch.de>
2020-07-09 14:23:45 +0200

    Add audio controls

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -2,13 +2,34 @@
 	<div>
 		<h1 class="display-1">404</h1>
 		<p class="display-5">Page not found</p>
-		<i class="rain" v-for="index in 144" :key="index"></i>
-		<a href="/" title="go back home">
-			<g-image src="~/assets/img/lost.png" width="256" class="shake" alt="Lost in space with rain" />
+		<i
+			class="rain"
+			v-for="index in 144"
+			:key="index"
+		></i>
+		<a
+			href="/"
+			title="go back home"
+		>
+			<g-image
+				src="~/assets/img/lost.png"
+				width="256"
+				class="shake"
+				alt="Lost in space with rain"
+			/>
 		</a>
 		<br />
-		<audio autoplay preload="none">
-			<source src="/assets/snd/rain.mp3" type="audio/mpeg" />
+		<audio
+			autoplay
+			preload="yes"
+			controls
+			tabindex="1"
+			title="Let it rain!"
+		>
+			<source
+				src="/assets/snd/rain.mp3"
+				type="audio/mpeg"
+			/>
 		</audio>
 	</div>
 </template>
@@ -16,7 +37,7 @@
 <script>
 export default {
 	metaInfo: {
-		title: 'Page not found'
+		title: "Page not found"
 	}
-}
+};
 </script>
